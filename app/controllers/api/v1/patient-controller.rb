8c8d2cfca56c7ebc3903aeb16c26b878faed7dab
unknown <swatifursule@gmail.com>
2018-08-09 18:12:20 +1000

    changed files

--- app/controllers/api/v1/patient-controller.rb
+++ app/controllers/api/v1/patient-controller.rb
@@ -1,8 +1,19 @@
 module Api::V1
   class PatientController < Api::V1::ApiController
-    def index
-      @patients = Patient.all
-      render json: @patients
+    def update
+      @patient = Patient.find(params[:id])
+      @patient.update_attributes(idea_params)
+      render json: @patient
     end
+
+    def index
+      @patient = Patient.all
+      render json: @patient
+    end
+    private
+
+  def patient_params
+    params.require(:patient).permit(:fName, :lName, :registrationNo, :dob, :status, :email, :address)
+  end
   end
 end
